81549b0fd5eec259c9b1104e2b2cb545c1dadf9c
Athyent <adrigl1996@gmail.com>
2018-01-24 12:44:59 +0100

    Se han añadido nuevas lineas

--- new.php
+++ new.php
@@ -4,4 +4,6 @@ curl_setopt($llamada, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($llamada, CURLOPT_CUSTOMREQUEST, "GET");
 $response = curl_exec($llamada);
 respuesta($response);
+
+echo "<ul><li></li><ul>";
 ?>
