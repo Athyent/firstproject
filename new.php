5efd0b067122a339f0294e8524a7882b63307149
Athyent <35689461+Athyent@users.noreply.github.com>
2018-01-24 12:20:34 +0100

    Create new.php
    Nuevo archivo

---
+++ new.php
@@ -0,0 +1,7 @@
+<?php
+$llamada = curl_init('http://localhost/Proyectos/ServiciosWeb/REST/Tienda/producto');
+curl_setopt($llamada, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($llamada, CURLOPT_CUSTOMREQUEST, "GET");
+$response = curl_exec($llamada);
+respuesta($response);
+?>
